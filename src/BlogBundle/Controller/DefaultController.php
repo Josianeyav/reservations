e4747ab3a1d8d28891174685db7d2ca9101830d6
Helianne <helianneyav@yahoo.fr>
2018-05-17 18:04:39 +0200

    upload project

---
+++ src/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BlogBundle:Default:index.html.twig');
+    }
+}
